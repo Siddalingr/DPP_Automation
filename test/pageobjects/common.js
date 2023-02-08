fc68da8806cc51181103ccd5c547aea6072b10a2
AnilCa1905 <114649299+AnilCa1905@users.noreply.github.com>
2023-02-08 15:27:24 +0530

    Update common.js

--- test/pageobjects/common.js
+++ test/pageobjects/common.js
@@ -6,8 +6,8 @@ this.$logo = () => $(`//img[@alt='Make My Trip']`)
   }
   async openUrl()
   {
-    await browser.url(`https://www.makemytrip.com/`);
+    await browser.url(`https://www.testingmavens.com/`);
     await browser.maximizeWindow();
-   await this.$homePageHeader().waitForDisplayed(10000,true,'Home pahe header is not displayed');
+   //await this.$homePageHeader().waitForDisplayed(10000,true,'Home pahe header is not displayed');
   }
 }
