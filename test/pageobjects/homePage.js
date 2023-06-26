e5296c3a5b8ed3bd1670a0558b36fe8dd164dc5c
AnilCa1905 <114649299+AnilCa1905@users.noreply.github.com>
2023-06-26 12:20:23 +0530

    Update homePage.js

--- test/pageobjects/homePage.js
+++ test/pageobjects/homePage.js
@@ -1,26 +1,11 @@
-const Common = require("../pageobjects/common");
-const testData = require("../testdata/testData.json");
+const Commom = require("./common");
 
-class HomePage extends Common {
-  constructor() {
-    super();
-    this.$mainModule = (icon) => $(`//span[contains(text(),"${icon}")]`);
-    this.$subModule = (icon) =>
-      $(`//span[text()='Men']//following::span[contains(text(),'${icon}')]`);
-    this.$subCatagory = (icon) => $(`//h1//span[contains(text(),"${icon}")]`);
-    //span[contains(text(),"Women")]
-  }
-
-  async moveToMainModule(element) {
-    await this.waitForLoad(this.$mainModule(element));
-    await this.$mainModule(element).moveTo();
-  }
-
-  async moveToSubModule(element) {
-    await this.waitForLoad(this.$subModule(element));
-    await this.$subModule(element).moveTo();
-  }
+class Homepage extends Commom {
+	constructor() {
+		super();
+		
+	}
 }
-module.exports = {
-  homePage: new HomePage(),
-};
+//exporting as Homepage object
+
+module.exports = { homePage : new Homepage () }
