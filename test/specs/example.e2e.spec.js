de75a695122ce5c90e17ff84a0bfebc3132d7f64
AnilCa1905 <114649299+AnilCa1905@users.noreply.github.com>
2023-07-28 15:42:11 +0530

    Update example.e2e.spec.js

--- test/specs/example.e2e.spec.js
+++ test/specs/example.e2e.spec.js
@@ -7,15 +7,10 @@ describe('QaWolf.com  ', () => {
 			await homePage.openUrl(env.baseUrl);
 			await expect(browser).toHaveUrl('https://www.qawolf.com/');
 		});
-		it('user should be able to select  industries and click on startups', async () => {
-			await homePage.startUps(testData.industries, testData.Startups);
-			expect(await homePage.$homePageHeader(testData.Startups).isDisplayed()).toBe(true, 'Expect startup page header to be displayed');
-			await expect(browser).toHaveUrl('https://www.qawolf.com/industries/startups');
-		});
-		it('user should be able to select  industries and click on Fintech', async () => {
-			await homePage.fineTech(testData.industries, testData.FineTech);
-			expect(await homePage.$homePageHeader(testData.FineTech).isDisplayed()).toBe(true, 'Expect startup page header to be displayed');
-			await expect(browser).toHaveUrl('https://www.qawolf.com/industries/fintech');
+		it('user should be able to Click on GET STARTED', async () => {
+			await homePage.getStarted(testData.getStarted,'Get started with QA Wolf');
+			expect(await homePage.$homePageHeader('Get started with QA Wolf').isDisplayed()).toBe(true, 'Expect startup page header to be displayed');
+			await expect(browser).toHaveUrl('https://www.qawolf.com/get-started');
 		});
 	});
 
